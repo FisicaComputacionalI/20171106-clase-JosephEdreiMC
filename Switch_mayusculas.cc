0f2c1e834e6ada5531ed6b11f29cc28e0868a803
JosephEdreiMC <joseph.edrei.mc@gmail.com>
2017-11-06 13:11:21 -0600

    Create Switch_mayusculas.cc

---
+++ Switch_mayusculas.cc
@@ -0,0 +1,21 @@
+//Uso de switch sin distincion en mayusculas                                                                                               
+//Joseph Edrei Moreno Cruz                                                                                     
+//30 de octubre del 2017                                                                                       
+#include <iostream>
+using namespace std;
+int main(){
+  char variable;
+  cout<<"escoge una opciòn entre A,B,C,D,F"<<endl;
+  cin>>variable;
+  variable=toupper(variable);
+
+  switch(variable){
+  case 'A' : cout<<"excelente"<<endl;break;
+  case 'B' : cout<<"muy bien"<<endl;break;
+  case 'C' : cout<<"bien"<<endl;break;
+  case 'D' : cout<<"pasate"<<endl;break;
+  case 'F' : cout<<"intentalo nuevamente"<<endl;break;
+  default : cout<<"opciòn no valida"<<endl;
+  }
+return 0;
+}
